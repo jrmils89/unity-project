d3a5646206af94bd1fa87b8b43202a9f30a3308f
Jesse Mills <jessemills1989@gmail.com>
2016-03-05 15:15:52 -0500

    Adding a category seed route
    Adding a seed route to seed some dummy data into the mongo DB. It can go to /categories/seed

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -15,5 +15,109 @@ router.post('/', function(req, res) {
   });
 })
 
+router.get('/seed', function(req, res) {
+  var data = [
+    {
+      title: 'ExpressJS',
+      concept: [
+        {
+          title: 'Router',
+          img: '',
+          stars: 2
+        },
+        {
+          title: 'Static',
+          img: '',
+          stars: 1
+        }
+      ]
+    },
+    {
+      title: 'Javascript',
+      concept: [
+        {
+          title: 'If...else',
+          img: '',
+          stars: 3
+        },
+        {
+          title: 'Loop',
+          img: '',
+          stars: 4
+        },
+        {
+          title: 'Function',
+          img: '',
+          stars: 8
+        }
+      ]
+    },
+    {
+      title: 'AngularJS',
+      concept: [
+        {
+          title: 'Controller',
+          img: '',
+          stars: 5
+        },
+        {
+          title: 'Directive',
+          img: '',
+          stars: 9
+        },
+        {
+          title: 'Module',
+          img: '',
+          stars: 8
+        }
+      ]
+    },
+    {
+      title: 'HTML',
+      concept: [
+        {
+          title: 'Elements',
+          img: '',
+          stars: 3
+        },
+        {
+          title: 'Script',
+          img: '',
+          stars: 2
+        },
+        {
+          title: 'Form',
+          img: '',
+          stars: 6
+        }
+      ]
+    },
+    {
+      title: 'CSS',
+      concept: [
+        {
+          title: 'Selectors',
+          img: '',
+          stars: 2
+        },
+        {
+          title: 'Anchors',
+          img: '',
+          stars: 3
+        },
+        {
+          title: 'Classes',
+          img: '',
+          stars: 2
+        }
+      ]
+    }
+  ];
+
+  Category.create(data, function(err, data) {
+    res.redirect('/categories')
+  });
+});
+
 
 module.exports = router;
