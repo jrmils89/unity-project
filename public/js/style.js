c66882b28a7bb6e9473f120c6004cbef3dcdff13
Levi Tardio <levitardio@192.168.1.5>
2016-03-07 23:06:03 -0500

    working on using jquery for css animations

---
+++ public/js/style.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+
+	$player2 = $('#player2');
+
+
+
+	
+	$addFirst.click(function() {
+
+	
+		$name1 = $('<p>');
+		
+		// so i can move the typed input value 
+		$name1.html($value1.val()); 
+		
+		$player1.append($name1);
+		
+		// console.log($player1);
+
+	});
+
+
+
+
+});
