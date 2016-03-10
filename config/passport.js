5ce2e5bdf37da2ebbfc16f78cb2f3784dea571e0
Jesse Mills <jessemills1989@gmail.com>
2016-03-10 09:10:38 -0500

    Commenting and code cleanup for passport config

--- config/passport.js
+++ config/passport.js
@@ -31,15 +31,12 @@ module.exports = function(passport) {
       // by default, local strategy uses username and password, we will override with email
       usernameField: 'email',
       passwordField: 'password',
-
       passReqToCallback: true // allows us to pass back the entire request to the callback
     },
     function(req, email, password, done) {
-
       // asynchronous
       // User.findOne wont fire unless data is sent back
       process.nextTick(function() {
-
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
         User.findOne({
@@ -62,7 +59,7 @@ module.exports = function(passport) {
             newUser.username = req.body.username;
             newUser.password = newUser.generateHash(password);
             newUser.save(function(err) {
-              if(err){
+              if (err) {
                 console.log(err)
                 throw err
               } else {
@@ -87,48 +84,45 @@ module.exports = function(passport) {
               return done(null, newUser);
             });
           }
-
         });
+      });
+    }));
 
+  // =========================================================================
+  // LOCAL LOGIN =============================================================
+  // =========================================================================
+  // we are using named strategies since we have one for login and one for signup
+  // by default, if there was no name, it would just be called 'local'
+
+  passport.use('local-login', new LocalStrategy({
+      // by default, local strategy uses username and password, we will override with email
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) { // callback with email and password from our form
+
+      // find a user whose email is the same as the forms email
+      // we are checking to see if the user trying to login already exists
+      User.findOne({
+        'email': email
+      }, function(err, user) {
+        // if there are any errors, return the error before anything else
+        if (err)
+          return done(err);
+
+        // if no user is found, return the message
+        if (!user)
+          return done(null, false);
+
+        // if the user is found but the password is wrong
+        if (!user.validPassword(password))
+          return done(null, false);
+
+        // all is well, return successful user
+        return done(null, user);
       });
 
-      }));
-
-      // =========================================================================
-      // LOCAL LOGIN =============================================================
-      // =========================================================================
-      // we are using named strategies since we have one for login and one for signup
-      // by default, if there was no name, it would just be called 'local'
-
-      passport.use('local-login', new LocalStrategy({
-          // by default, local strategy uses username and password, we will override with email
-          usernameField: 'email',
-          passwordField: 'password',
-          passReqToCallback: true // allows us to pass back the entire request to the callback
-        },
-        function(req, email, password, done) { // callback with email and password from our form
-
-          // find a user whose email is the same as the forms email
-          // we are checking to see if the user trying to login already exists
-          User.findOne({
-            'email': email
-          }, function(err, user) {
-            // if there are any errors, return the error before anything else
-            if (err)
-              return done(err);
-
-            // if no user is found, return the message
-            if (!user)
-              return done(null, false); // req.flash is the way to set flashdata using connect-flash
-
-            // if the user is found but the password is wrong
-            if (!user.validPassword(password))
-              return done(null, false); // create the loginMessage and save it to session as flashdata
-
-            // all is well, return successful user
-            return done(null, user);
-          });
-
-        }));
+    }));
 
 };
