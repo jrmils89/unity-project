71f3f033f9468535333aeb6e19a685e1a2d1ecfd
Jesse Mills <jessemills1989@gmail.com>
2016-03-10 09:08:12 -0500

    Commenting and code cleanup for category controller

--- public/js/controllers/categoryController.js
+++ public/js/controllers/categoryController.js
@@ -4,41 +4,46 @@ var app = angular.module('categoryContoller', ['ngCookies']);
 app.controller('categoryContoller', ['$http','$location','$cookies', '$scope', function($http,$location,$cookies, $scope) {
   var self = this;
 
+  // ==================================================================================
   // This gets a cookie that was set by the server if the user requested a path other than '/'
   // If it finds that cookie and it's not either null or '/favicon' then it will redirect the window
   // to the requested path. The '/favicon' part is a placeholder in there until the application has a favicon...
-
   var path = $cookies.get('redirectUrlFlowLy');
 
   if(path != 'null' && path != '/favicon.ico') {
     $cookies.put('redirectUrlFlowLy','null');
     $location.path(path);
   }
+  // ==================================================================================
 
+  // Declaring default values
   this.categoryNames = null;
-
   this.show = false;
+  this.newCategory = {};
 
-  this.revealCategories = function(){
-    self.show = !self.show
+  // This is a function to show/hide the categories
+  this.revealCategories = function() {
+    self.show = !self.show;
   };
 
   // Makes a GET request to the server /categories route
   this.loadData = function() {
       $http.get('/api/v1/categories').then(
       function(response) {
+        // Sets the category names to the response data
         self.categoryNames = response.data;
       },
       function(error) {
-        console.log(error);
+        console.log(error); // Logs the error
       }
     );
   };
 
+  // Call the load data function by default, which loads all of our categories
   this.loadData();
 
-  this.newCategory = {}
-
+  // NEW
+  // Makes a POST request to our endpoint to add a category
   this.addCategory = function(){
     $http({
       method:"POST",
@@ -46,17 +51,14 @@ app.controller('categoryContoller', ['$http','$location','$cookies', '$scope', f
       data: this.newCategory
     }).then(
       function(response){
+        // Clears out the object holding the new category
+        // and then reloads all the data on the page
         self.newCategory = {};
         self.loadData();
       },
       function(error){
-        console.log(error)
-      }
-    )
-  }
-
-
-
+        console.log(error); // Logs the error
+      };
+    );
+  };
 }]);
-
-
