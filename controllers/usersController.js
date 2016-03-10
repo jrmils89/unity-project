a43ae9b47c222074139ecd6f0bdf5ffee9911b54
Levi Tardio <levitardio@Levis-MacBook-Pro.local>
2016-03-10 14:35:56 -0500

    commented addConcept, users (fron/back end) and draw controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,23 +1,29 @@
+// using express to pass data from and to client
 var express = require('express');
 var router  = express.Router();
 var User = require('../models/user.js');
 var passport = require('passport');
 
-
+// if you are an admin, express will find and display a list of all of the users 
 router.get('/', isAdmin, function(req, res) {
   User.find({}, function(err, data) {
     res.json(data);
   });
 });
 
+
+// if you are a new user and sign-up, passport will authenticate that information
 router.post('/signup', passport.authenticate('local-signup', {failureRedirect: '/' }), function(req, res) {
+    // that user information is now being requested by the cookies API
     res.cookie('userid', req.user.id);
     res.cookie('userUsername', req.user.username);
     res.cookie('userEmail', req.user.email);
     res.cookie('userIsAdmin', req.user.isAdmin);
+    // sending the successfully created user data to the browser
     res.json({success: true});
   }
 );
+
 
 router.post('/login', passport.authenticate('local-login', {failureMessage: 'fail'}), function(req, res) {
     // When the user successfully logs 3 different cookies are set, that will be user on the front end side
--- public/js/controllers/drawController.js
+++ public/js/controllers/drawController.js
@@ -1,23 +1,38 @@
 var app = angular.module("drawController", []);
 
+// creating a controller for the drawing directive 
 app.controller("drawController", [function(){
 	var self = this;
 
+	// initializing / instantiating / decalaring the variable 'text' & groupOne
+	// as null because the user has not drawn anything yet 
 	this.text = null;
+	// same thing but the variable that will be used when 
+	// a user groups text with shapes (referencing ng-model in the draw.html input fields)
 	this.groupOne = null;
 
+	// retrieving the element <canvas> by it's id, 'canvas' in order to
+	// manipulate the DOM within the draw.html partial
 	this.canvas = document.getElementById('canvas');
+	// storing the '2d' data inside the ctx variable we created
 	this.ctx = document.getElementById('canvas').getContext('2d');
 
+	// using google's built in data attributes 
 	this.ctx.fillStyle = "white";
 	this.ctx.fillRect(0,0,self.canvas.width,self.canvas.height);
 	this.ctx.fillStyle = "black";
 
+	// creating a function that will activate once a user chooses 
+	// an item to use in their canvas
 	this.addElToCanvas = function(e) {
+		// if the user's group consists of text and a rectangle,
+		// add text to the rectangle
 		if (self.groupOne == 'text' && self.checkRectangle) {
 			self.addText(e);
 			self.rectangle(e);
+		// same logic applies for if the user only wants the shape without text
 		}  else if (self.checkRectangle) {
+			// just return the rectangle
 			self.rectangle(e);
 		} else if (self.groupOne =='text') {
 			self.addText(e);
@@ -32,14 +47,22 @@ app.controller("drawController", [function(){
 		}
 	};
 
+	// function that is invoked in the addElToCanvas function when a user 
+	// elects to add text to their canvas
 	this.addText = function(e) {
+		// if user did not enter text, the text remains empty
 		if (self.text == null) {self.text = ''};
+		// setting the defaut font for text
 		self.ctx.font = "12px sans-serif";
+		// the text will be placed wherever the user clicks
 		self.ctx.fillText(self.text, e.layerX+5, e.layerY+25, 95);
 	};
 
+	// all of the shapes uses the same logic in terms of where they will be placed
+	// on the canvas
 	this.rightArrow = function(e) {
     self.ctx.beginPath();
+    	// except you will be able to move the shapes 
 		self.ctx.moveTo(e.layerX, e.layerY);
 		self.ctx.lineTo(e.layerX+50, e.layerY);
 		self.ctx.lineTo(e.layerX+40, e.layerY-10);
@@ -88,6 +111,7 @@ app.controller("drawController", [function(){
 		self.ctx.stroke();
 	};
 
+	// if the user clicks the 'clear' button, it will trigger 'this.clear()'
 	this.clear = function() {
 		self.ctx.clearRect(0,0,self.canvas.width, self.canvas.height);
 		this.ctx.fillStyle = "white";
@@ -95,11 +119,17 @@ app.controller("drawController", [function(){
 		this.ctx.fillStyle = "black";
 	}
 
+	// if user clicks on the button that triggers this click function
 	this.download = function() {
+		// the browser will access it's built-in function - toDataURL()
+		// which will process the user's canvas and convert the image into 
+		// a URL
 		var dt = canvas.toDataURL();
 		this.href = dt;
 	};
 
+	// creating a click function and attaching it to the download button
+	// which will listen for when the user clicks 'download'
 	document.getElementById('download').addEventListener('click', this.download, false);
 
 }])
